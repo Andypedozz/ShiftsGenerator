87e9c78a24b5b5d29c8c04458d4cdb547e5f2583
andrea <andypedini02@gmail.com>
2025-03-06 22:24:22 +0100

    .

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const fs = require('fs');
 const path = require("path");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "/")));
@@ -122,6 +122,10 @@ function calc() {
     schedule = generateMonthlySchedule(config);
     count = printCountShifts(schedule,config)
 }
+
+app.get("/", (req, res) => {
+    res.redirect("/gui");
+});
 
 app.get("/schedule", (req, res) => {
     res.json(schedule);
